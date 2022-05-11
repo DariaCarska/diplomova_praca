b8806007a4703ed56dd99d68c4f46802b497fd3d
Daria Carska <daria.carska@gmail.com>
2022-05-11 22:34:46 +0200

    train and save model

--- scripts/train_gan.py
+++ scripts/train_gan.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # %%
-# example of training a gan on mnist
 import numpy as np
 from numpy import expand_dims
 from numpy import zeros
@@ -11,7 +10,7 @@ from numpy.random import randn
 from numpy.random import randint
 from keras.datasets.mnist import load_data
 from tensorflow.keras.optimizers import Adam
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Dense
 from keras.layers import Reshape
 from keras.layers import Flatten
@@ -25,7 +24,6 @@ from data_preprocessing import preprocess_data
 import os
 
 #%%
-# define the standalone discriminator model
 def define_discriminator(in_shape=(WIDTH,HEIGHT,1)):
 	model = Sequential()
 	model.add(Conv2D(64, (3,3), strides=(2, 2), padding='same', input_shape=in_shape))
@@ -36,51 +34,35 @@ def define_discriminator(in_shape=(WIDTH,HEIGHT,1)):
 	model.add(Dropout(0.4))
 	model.add(Flatten())
 	model.add(Dense(1, activation='sigmoid'))
-	# compile model
 	opt = Adam(lr=0.0002, beta_1=0.5)
 	model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
 	return model
 #%%
-# define the standalone generator model
 def define_generator(latent_dim):
 	model = Sequential()
-	# foundation for 32x32 image
 	n_nodes = 128 * 8 * 8
-
 	
 	model.add(Dense(n_nodes, input_dim=latent_dim))
 	model.add(LeakyReLU(alpha=0.2))
 	model.add(Reshape((8, 8, 128)))
 	for i in range(3):
-		# upsample to 64x64
+		# upsample
 		model.add(Conv2DTranspose(128, (4,4), strides=(2,2), padding='same'))
 		model.add(LeakyReLU(alpha=0.2))
-	# upsample to 128x128
-	# model.add(Conv2DTranspose(128, (4,4), strides=(2,2), padding='same'))
-	# model.add(LeakyReLU(alpha=0.2))
-	# upsample to 256x256
-	# model.add(Conv2DTranspose(128, (4,4), strides=(2,2), padding='same'))
-	# model.add(LeakyReLU(alpha=0.2))
+
 
 	model.add(Conv2D(1, (8,8), activation='sigmoid', padding='same'))
 	return model
 
-# define the combined generator and discriminator model, for updating the generator
 def define_gan(g_model, d_model):
-	# make weights in the discriminator not trainable
 	d_model.trainable = False
-	# connect them
 	model = Sequential()
-	# add generator
 	model.add(g_model)
-	# add the discriminator
 	model.add(d_model)
-	# compile model
 	opt = Adam(lr=0.0002, beta_1=0.5)
 	model.compile(loss='binary_crossentropy', optimizer=opt)
 	return model
 # %%
-# load and prepare mnist training images
 def load_real_samples():
 	num_samples = 0
 	for dir in os.listdir('../data/Invisalign'):
@@ -94,112 +76,66 @@ def load_real_samples():
 				X[i,:,:,0] = preprocess_data(os.path.join('../data/Invisalign', dir, sample))
 				i += 1
 
-	# load mnist dataset
-	# (trainX, _), (_, _) = load_data()
-	# expand to 3d, e.g. add channels dimension
-	# X = expand_dims(trainX, axis=-1)
-	# convert from unsigned ints to floats
 	X = X.astype('float32')
-	# scale from [0,255] to [0,1]
 	X = X / 255.0
 	return X
 # %%
-# select real samples
 def generate_real_samples(dataset, n_samples):
-	# choose random instances
 	ix = randint(0, dataset.shape[0], n_samples)
-	# retrieve selected images
 	X = dataset[ix]
-	# generate 'real' class labels (1)
 	y = ones((n_samples, 1))
 	return X, y
 
-# generate points in latent space as input for the generator
 def generate_latent_points(latent_dim, n_samples):
-	# generate points in the latent space
 	x_input = randn(latent_dim * n_samples)
-	# reshape into a batch of inputs for the network
 	x_input = x_input.reshape(n_samples, latent_dim)
 	return x_input
 
-# use the generator to generate n fake examples, with class labels
 def generate_fake_samples(g_model, latent_dim, n_samples):
-	# generate points in latent space
 	x_input = generate_latent_points(latent_dim, n_samples)
-	# predict outputs
 	X = g_model.predict(x_input)
-	# create 'fake' class labels (0)
 	y = zeros((n_samples, 1))
 	return X, y
 
-# create and save a plot of generated images (reversed grayscale)
 def save_plot(examples, epoch, n=10):
-	# plot images
 	for i in range(n * n):
-		# define subplot
 		pyplot.subplot(n, n, 1 + i)
-		# turn off axis
 		pyplot.axis('off')
-		# plot raw pixel data
 		pyplot.imshow(examples[i, :, :, 0], cmap='gray_r')
-	# save plot to file
 	filename = 'generated_plot_e%03d.png' % (epoch+1)
 	pyplot.savefig(filename)
 	pyplot.close()
 
-# evaluate the discriminator, plot generated images, save generator model
 def summarize_performance(epoch, g_model, d_model, dataset, latent_dim, n_samples=100):
-	# prepare real samples
 	X_real, y_real = generate_real_samples(dataset, n_samples)
-	# evaluate discriminator on real examples
 	_, acc_real = d_model.evaluate(X_real, y_real, verbose=0)
-	# prepare fake examples
 	x_fake, y_fake = generate_fake_samples(g_model, latent_dim, n_samples)
-	# evaluate discriminator on fake examples
 	_, acc_fake = d_model.evaluate(x_fake, y_fake, verbose=0)
-	# summarize discriminator performance
 	print('>Accuracy real: %.0f%%, fake: %.0f%%' % (acc_real*100, acc_fake*100))
-	# save plot
 	save_plot(x_fake, epoch)
-	# save the generator model tile file
 	filename = 'generator_model_%03d.h5' % (epoch + 1)
 	g_model.save(filename)
 
-# train the generator and discriminator
 def train(g_model, d_model, gan_model, dataset, latent_dim, n_epochs=100, batch_size=32):
 	n_batch = int(dataset.shape[0] / batch_size)
 	half_batch = int(n_batch / 2)
-	# manually enumerate epochs
 	for i in range(n_epochs):
-		# enumerate batches over the training set
 		for j in range(batch_size):
-			# get randomly selected 'real' samples
 			X_real, y_real = generate_real_samples(dataset, half_batch)
-			# generate 'fake' examples
 			X_fake, y_fake = generate_fake_samples(g_model, latent_dim, half_batch)
-			# create training set for the discriminator
 			X, y = vstack((X_real, X_fake)), vstack((y_real, y_fake))
-			# update discriminator model weights
 			d_loss, _ = d_model.train_on_batch(X, y)
-			# prepare points in latent space as input for the generator
 			X_gan = generate_latent_points(latent_dim, n_batch)
-			# create inverted labels for the fake samples
 			y_gan = ones((n_batch, 1))
-			# update the generator via the discriminator's error
 			g_loss = gan_model.train_on_batch(X_gan, y_gan)
-			# summarize loss on this batch
 			print('>%d, %d/%d, d=%.3f, g=%.3f' % (i+1, j+1, batch_size, d_loss, g_loss))
-		# evaluate the model performance, sometimes
 		if (i+1) % 10 == 0:
 			summarize_performance(i, g_model, d_model, dataset, latent_dim)
 
 # %% size of the latent space
 latent_dim = 100
-# create the discriminator
 d_model = define_discriminator()
-# create the generator
 g_model = define_generator(latent_dim)
-# create the gan
 gan_model = define_gan(g_model, d_model)
 # %% load image data
 dataset = load_real_samples()
@@ -207,4 +143,13 @@ dataset = load_real_samples()
 # %% train model
 train(g_model, d_model, gan_model, dataset, latent_dim)
 
+# %% save model
+g_model.save("../models/generator")
+# %%
+model = load_model("../models/generator")
+# %%
+X, y = generate_fake_samples(model, 100, 1)
+# %%
+pyplot.imshow(X[0,:,:,0], cmap='gray')
+pyplot.savefig("../plots/fake_sample1.png")
 # %%
